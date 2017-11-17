4fc4501056665ef185e2c811ae7503b89d3d53b1
Benoit Vermont <bvermont@yelp.com>
2017-11-17 14:08:19 +0100

    Working with buttons

--- app/src/main/java/com/example/androidthings/peripherals/HomeActivity.kt
+++ app/src/main/java/com/example/androidthings/peripherals/HomeActivity.kt
@@ -4,7 +4,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
 import com.google.android.things.pio.Gpio
 import com.google.android.things.pio.PeripheralManagerService
 import com.google.firebase.database.DatabaseReference
@@ -25,11 +26,13 @@ class HomeActivity : AppCompatActivity() {
         private val TAG = "HomeActivity"
         private val LED_PIN = "GPIO_10"
         private val PUMP_PIN = "GPIO_39"
+        private val DISARMED_PIN = "GPIO_173"
     }
 
     // GPIO connection to button input
-    private var mLed: Gpio? = null
-    private var mPump: Gpio? = null
+    private var led: Gpio? = null
+    private var disarmLed: Gpio? = null
+    private var pump: Gpio? = null
     private val mcp3008 = MCP3008(CS_PIN, CLOCK_PIN, MOSI_PIN, MISO_PIN)
 
     private var mIsRunning = true
@@ -44,14 +47,17 @@ class HomeActivity : AppCompatActivity() {
     private var mValueRef: DatabaseReference? = null
     private var mGardeningRef: DatabaseReference? = null
 
-    private var mMainView: View? = null
-    private var mWaterView: WaterLevelView? = null
+    private var waterLevel: Float = 0.0f
+    private var isDisarmed: Boolean = false
+
+    private lateinit var mWaterView: WaterLevelView
+    private lateinit var mCloudView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        mMainView = findViewById(R.id.main_view)
         mWaterView = findViewById(R.id.water_level)
+        mCloudView = findViewById(R.id.cloud)
 
         // Firebase Setup
         mDatabase = FirebaseDatabase.getInstance()
@@ -68,13 +74,24 @@ class HomeActivity : AppCompatActivity() {
         } catch (e: IOException) {
             Log.w(TAG, "Error initializing mcp3008", e)
         }
-        mLed = service.openGpio(LED_PIN)
-        mLed!!.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
-        mPump = service.openGpio(PUMP_PIN)
-        mPump!!.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+        led = service.openGpio(LED_PIN)
+        led!!.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+        disarmLed = service.openGpio(DISARMED_PIN)
+        disarmLed!!.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+        pump = service.openGpio(PUMP_PIN)
+        pump!!.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
 
         // Start the main event Loop
         mEventHandler.post(mEventLoopRunnable)
+
+
+        findViewById<ImageButton>(R.id.start).setOnClickListener {
+            water()
+        }
+
+        findViewById<ImageButton>(R.id.stop).setOnClickListener {
+            toggle()
+        }
     }
 
     private var mEventLoopRunnable = object : Runnable {
@@ -86,12 +103,12 @@ class HomeActivity : AppCompatActivity() {
             mStatusRef?.setValue("reading")
 
             val readAdc0 = mcp3008.readAdc(0)
-            val convertedValue = convertAdcValue(readAdc0)
+            waterLevel = convertAdcValue(readAdc0)
 
-            mLed?.value = convertedValue > 0.5f
-            mWaterView?.value = 1.0f - convertedValue
+            led?.value = shouldPump()
+            mWaterView.value = 1.0f - waterLevel
 
-            Log.d(TAG, "Pump value = ${mLed!!.value}, Adc values: channel 0 = $readAdc0, to float = $convertedValue")
+            Log.d(TAG, "Pump value = ${led!!.value}, Adc values: channel 0 = $readAdc0, to float = $waterLevel")
 
 
             if (mIsRunning) {
@@ -101,37 +118,34 @@ class HomeActivity : AppCompatActivity() {
                 mCounter += 60
                 if (mCounter >= 1000) {
                     mCounter = 0
-                    mValueRef?.setValue(convertedValue)
+                    mValueRef?.setValue(waterLevel)
                 }
             }
 
-            if (convertedValue > 0.5f && !mIsPumping) {
-                mStatusRef?.setValue("pumping")
-                Log.d(TAG, "Staring to pump")
-
-                mIsPumping = true
-                mPump!!.value = true
-                mPumpHandler.postDelayed(mStopPumpRunnable, 1000)
+            if (shouldPump() && !mIsPumping) {
+                water()
             }
         }
     }
 
     private var mStopPumpRunnable = Runnable {
-        mPump!!.value = false
+        pump!!.value = false
         mIsPumping = false
         Log.d(TAG, "Stopping to pump")
 
         val dateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.US)
         val date = dateFormat.format(Date())
         mGardeningRef?.child(System.currentTimeMillis().toString())?.setValue(date)
+        updateCloudState()
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
         mcp3008.unregister()
-        mLed?.close()
-        mPump?.close()
+        led?.close()
+        disarmLed?.close()
+        pump?.close()
 
         mIsRunning = false
     }
@@ -139,5 +153,54 @@ class HomeActivity : AppCompatActivity() {
     private fun convertAdcValue(value: Int): Float {
         val caped = minOf(maxOf(value - 400, 0), 623)
         return caped.toFloat() / 623f * 1.0f
+    }
+
+    fun shouldPump(): Boolean {
+        if (isDisarmed) {
+            return false
+        }
+        return waterLevel > 0.5f
+    }
+
+
+    fun disarm() {
+        isDisarmed = true
+        disarmLed?.value = true
+        updateCloudState()
+    }
+
+    fun arm() {
+        isDisarmed = false
+        disarmLed?.value = false
+        updateCloudState()
+    }
+
+    private fun updateCloudState() {
+        if (mIsPumping) {
+            mCloudView.setImageResource(R.drawable.ic_cloud_with_rain)
+        } else if (isDisarmed) {
+            mCloudView.setImageResource(R.drawable.ic_cloud_off)
+        } else {
+            mCloudView.setImageResource(R.drawable.ic_cloud_no_rain)
+        }
+    }
+
+
+    fun toggle() {
+        if (isDisarmed){
+            arm()
+        } else{
+            disarm()
+        }
+    }
+
+    fun water() {
+        mStatusRef?.setValue("pumping")
+        Log.d(TAG, "Staring to pump")
+
+        mIsPumping = true
+        pump!!.value = true
+        mPumpHandler.postDelayed(mStopPumpRunnable, 1000)
+        updateCloudState()
     }
 }
